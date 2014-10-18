97a8a8571490c520cb9c5ab53ed4b26ef2f06ee7
Stefan Edwards <saedwards.ecc@gmail.com>
2014-10-18 12:57:16 -0400

    more pep8 love, with more to come

--- plugins/pyscrape.py
+++ plugins/pyscrape.py
@@ -10,70 +10,70 @@ import datetime
 
 
 def downloader(url,filename):
-	patternFile = open('pSearch.dat','r').read().splitlines()
-	data = ""
-	try:
-		try:
-			data = urllib.urlopen(url).read()
-		except:
-			print "Error connecting to pastebin!"
-			raise
-		for pattern in patternFile:
-			m = re.findall("'"+pattern+"'",data)
-			if m:
-				print ""
-				print "Found Match"
-				print "Expr:"+pattern
-				print ""
-				now = datetime.datetime.now()
-				hardFile = "pastebin/downloaded/"+filename+"-"+str(time.ctime())+".txt"
-				file = open(hardFile,'w')
-				file.write(data)
-				file.close
-			else:
-				continue
-	except:
-		#print "There was a file IO error"
-		pass
+    patternFile = open('pSearch.dat','r').read().splitlines()
+    data = ""
+    try:
+        try:
+            data = urllib.urlopen(url).read()
+        except:
+            print "Error connecting to pastebin!"
+            raise
+        for pattern in patternFile:
+            m = re.findall("'"+pattern+"'",data)
+            if m:
+                print ""
+                print "Found Match"
+                print "Expr:"+pattern
+                print ""
+                now = datetime.datetime.now()
+                hardFile = "pastebin/downloaded/"+filename+"-"+str(time.ctime())+".txt"
+                file = open(hardFile,'w')
+                file.write(data)
+                file.close
+            else:
+                continue
+    except:
+        #print "There was a file IO error"
+        pass
 
 def starter():
-	if not os.path.exists("pastebin"):
-		os.mkdir("pastebin")
-		os.mkdir("pastebin/downloaded")
-	if not os.path.exists("pastebin/seen-pastes.txt"):
-		chkf = open("pastebin/seen-pastes.txt",'w')
-		chkf.close()
-	print "Scraping for pastes..."
-	while True:
-		try:
-			#open the pastebin archive
-			data = urllib.urlopen("http://pastebin.com/archive").read()
-			data = data.split('<table class="maintable" cellspacing="0">')
-			data = data[1]
-			data = data.split('</table>')
-			data = data[0]
+    if not os.path.exists("pastebin"):
+        os.mkdir("pastebin")
+        os.mkdir("pastebin/downloaded")
+    if not os.path.exists("pastebin/seen-pastes.txt"):
+        chkf = open("pastebin/seen-pastes.txt",'w')
+        chkf.close()
+    print "Scraping for pastes..."
+    while True:
+        try:
+            #open the pastebin archive
+            data = urllib.urlopen("http://pastebin.com/archive").read()
+            data = data.split('<table class="maintable" cellspacing="0">')
+            data = data[1]
+            data = data.split('</table>')
+            data = data[0]
 
-			data = data.replace('<td><img src="/i/t.gif"  class="i_p0" alt="" border="0" /><a href="/', "!!HTML!!")
-			data = data.replace('</a>', "!!HTML!!")
-			data = data.split("!!HTML!!")
-			for i in data:
-				i = i.split("\">")
-				i = i[0]
-				if not (("</td>" in i) or ("<tr class=" in i)):
-					seenf = open("pastebin/seen-pastes.txt",'r').read().splitlines()
-					if not ( i in seenf):
-						#print i
-						seenf2 = open("pastebin/seen-pastes.txt",'a')
-						seenf2.write(i)
-						seenf2.write("\n")
-						seenf2.close()
-						i2 = i
-						i = ("http://pastebin.com/raw.php?i="+i)
-						thread.start_new_thread(downloader, (i,i2))
-						time.sleep(4)
-		except:
-			print "Exiting..."
-			pass
-			raise
+            data = data.replace('<td><img src="/i/t.gif"  class="i_p0" alt="" border="0" /><a href="/', "!!HTML!!")
+            data = data.replace('</a>', "!!HTML!!")
+            data = data.split("!!HTML!!")
+            for i in data:
+                i = i.split("\">")
+                i = i[0]
+                if not (("</td>" in i) or ("<tr class=" in i)):
+                    seenf = open("pastebin/seen-pastes.txt",'r').read().splitlines()
+                    if not ( i in seenf):
+                        #print i
+                        seenf2 = open("pastebin/seen-pastes.txt",'a')
+                        seenf2.write(i)
+                        seenf2.write("\n")
+                        seenf2.close()
+                        i2 = i
+                        i = ("http://pastebin.com/raw.php?i="+i)
+                        thread.start_new_thread(downloader, (i,i2))
+                        time.sleep(4)
+        except:
+            print "Exiting..."
+            pass
+            raise
 def stopper():
-	raise
+    raise
