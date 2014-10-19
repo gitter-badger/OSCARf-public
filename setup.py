b17d87044da35ed2de16dcd87ee94a2c80def58e
NinJaSl0th <sl0th@force-sec.com>
2014-10-19 11:35:56 -0400

    pep8 love added to setup

--- setup.py
+++ setup.py
@@ -7,74 +7,75 @@ from time import sleep
 import sys
 
 if not os.path.exists("auth"):
-  print "Adding the 'auth' directory. Be sure to run the two setup scripts after this one!!!"
-  try:
-    os.mkdir("auth")
-  except:
-    exit("run as sudo!")
+    print "Adding the 'auth' directory. Be sure to run the two setup scripts after this one!!!"
+    try:
+        os.mkdir("auth")
+    except:
+        exit("run as sudo!")
 if not os.path.isfile('auth/twitter_app.dat'):
-  try:
-    appfile = open('auth/twitter_app.dat','w')
-    appfile.write("#App name\n")
-    appname = raw_input("Enter twitter app name: ")
-    appfile.write(appname+'\n')
-    appfile.write("#Consumer key\n")
-    appconsumer = raw_input("Enter twitter app consumer key: ")
-    appfile.write(appconsumer+'\n')
-    appfile.write("#Consumer Secret\n")
-    appsecret = raw_input("Enter twitter app secret key: ")
-    appfile.write(appsecret+'\n')
-    appfile.close()
-    sleep(1)
-    exit("Restart the setup script!")
-  except:
-    print "There was an issue creating the twitter app auth data!!"
-    exit()
+    try:
+        appfile = open('auth/twitter_app.dat','w')
+        appfile.write("#App name\n")
+        appname = raw_input("Enter twitter app name: ")
+        appfile.write(appname+'\n')
+        appfile.write("#Consumer key\n")
+        appconsumer = raw_input("Enter twitter app consumer key: ")
+        appfile.write(appconsumer+'\n')
+        appfile.write("#Consumer Secret\n")
+        appsecret = raw_input("Enter twitter app secret key: ")
+        appfile.write(appsecret+'\n')
+        appfile.close()
+        sleep(1)
+        exit("Restart the setup script!")
+    except:
+        print "There was an issue creating the twitter app auth data!!"
+        exit()
 
 
 try:
-  import twitter
-  from twitter.oauth import write_token_file, read_token_file
-  from twitter.oauth_dance import oauth_dance
+    import twitter
+    from twitter.oauth import write_token_file, read_token_file
+    from twitter.oauth_dance import oauth_dance
 except:
-  print "[+]ERROR: Unable to import the twitter library! Run Dependency Check!!"
-  exit()
+    print "[+]ERROR: Unable to import the twitter library! Run Dependency Check!!"
+    exit()
 
 
 #Authentication information for the application
 #Open file for twitter app auth
 try:
-  tappfile = open('auth/'+'twitter_app.dat','r')
-  tappline = tappfile.readlines()
-  APP_NAME = tappline[1].rstrip()
-  CONSUMER_KEY = tappline[3].rstrip()
-  CONSUMER_SECRET = tappline[5].rstrip()
-  tappfile.close()
+    tappfile = open('auth/'+'twitter_app.dat','r')
+    tappline = tappfile.readlines()
+    APP_NAME = tappline[1].rstrip()
+    CONSUMER_KEY = tappline[3].rstrip()
+    CONSUMER_SECRET = tappline[5].rstrip()
+    tappfile.close()
 except:
-  exit("Could not read app data file for twitter")
+    exit("Could not read app data file for twitter")
 
 #file that Oauth data is stored
 TOKEN_FILE = 'auth/'+'token.txt'
 #i = open(TOKEN_FILE,'w')
 #i.close()
 try:
-        (oauth_token, oauth_token_secret) = read_token_file(TOKEN_FILE)
+    (oauth_token, oauth_token_secret) = read_token_file(TOKEN_FILE)
 except IOError, e:
-        (oauth_token, oauth_token_secret) = oauth_dance(APP_NAME, CONSUMER_KEY,CONSUMER_SECRET)
-        print e.errno
-        print e
-        write_token_file(TOKEN_FILE, oauth_token, oauth_token_secret)
+    (oauth_token, oauth_token_secret) = oauth_dance(
+        APP_NAME, CONSUMER_KEY,CONSUMER_SECRET)
+    print e.errno
+    print e
+    write_token_file(TOKEN_FILE, oauth_token, oauth_token_secret)
 
 print "Creating shodan auth file"
 if not os.path.exists("auth"):
-  exit("auth directory not found")
+    exit("auth directory not found")
 
 
 if not os.path.isfile('auth/twitter_app.dat'):
-  exit('Shodan auth is already present!')
+    exit('Shodan auth is already present!')
 else:
-  f = open('auth/'+'shodankey.txt','w')
-  key = raw_input('Shodan API key: ')
-  f.write('#Shodan API key\n')
-  f.write(key+'\n')
-  f.close()
+    f = open('auth/'+'shodankey.txt','w')
+    key = raw_input('Shodan API key: ')
+    f.write('#Shodan API key\n')
+    f.write(key+'\n')
+    f.close()
