ebc05a3e6bb484ff145598cc7d596b1db3e5c7f8
Stefan Edwards <saedwards.ecc@gmail.com>
2014-10-18 21:18:15 -0400

    more pep8 love, minor tweaks

--- plugins/instag.py
+++ plugins/instag.py
@@ -2,22 +2,23 @@
 
 import urllib2
 
+
 def checker(usernom):
     #use username from FB profile and try it against instagram
     inst = "http://instagram.com/"+usernom
     try:
         data = urllib2.urlopen(inst)
-        valid = 1
+        valid = True
         #data = data.read()
         #print data
         #If we get a 404/not found, then set valid to 0
     except urllib2.HTTPError:
-        valid = 0
+        valid = False
     if valid:
         print
         print "---------"
         print "Found "+usernom+" on instagram!"
         print "Profile link: http://instagram.com/"+usernom
         print "---------\n"
-    elif valid == False:
+    elif not valid:
         return
